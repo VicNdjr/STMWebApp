977319a823ea9716f21921e0b29baa81784fc36e
Noémie <nlecocq@student.uliege.be>
2019-09-27 13:52:48 -0400

    Reformat

--- dataExtractor.js
+++ dataExtractor.js
@@ -1,34 +1,26 @@
-
 function displaylines() {
-    for (var i = 0; i < lines.length; i ++) {
+    for (var i = 0; i < lines.length; i++) {
         document.getElementById("allLines").innerHTML += '<li id="line-' + lines[i].id + '-' +
             lines[i].direction + '" onclick="openLine(this.id)">' + lines[i].id + ' ' + lines[i].name + ' (' +
             lines[i].direction + ')' + '</li>';
 
-        if (lines[i].category === 'local'){
+        if (lines[i].category === 'local') {
             document.getElementById("localLines").innerHTML += '<li id="line-' + lines[i].id + '-' +
                 lines[i].direction + '" onclick="openLine(this.id)">' + lines[i].id + ' ' + lines[i].name + ' (' +
                 lines[i].direction + ')' + '</li>';
-        }
-
-        else if (lines[i].category === 'night'){
+        } else if (lines[i].category === 'night') {
             document.getElementById("nightLines").innerHTML += '<li id="line-' + lines[i].id + '-' +
                 lines[i].direction + '" onclick="openLine(this.id)">' + lines[i].id + ' ' + lines[i].name + ' (' +
                 lines[i].direction + ')' + '</li>';
-        }
-        else if (lines[i].category === 'express'){
+        } else if (lines[i].category === 'express') {
             document.getElementById("expressLines").innerHTML += '<li id="line-' + lines[i].id + '-' +
                 lines[i].direction + '" onclick="openLine(this.id)">' + lines[i].id + ' ' + lines[i].name + ' (' +
                 lines[i].direction + ')' + '</li>';
-        }
-
-        else if (lines[i].category === 'dedicated'){
+        } else if (lines[i].category === 'dedicated') {
             document.getElementById("shuttleLines").innerHTML += '<li id="line-' + lines[i].id + '-' +
                 lines[i].direction + '" onclick="openLine(this.id)">' + lines[i].id + ' ' + lines[i].name + ' (' +
                 lines[i].direction + ')' + '</li>';
-        }
-
-        else if (lines[i].category === 'shuttleOr'){
+        } else if (lines[i].category === 'shuttleOr') {
             document.getElementById("shuttleOrLines").innerHTML += '<li id="line-' + lines[i].id + '-' +
                 lines[i].direction + '" onclick="openLine(this.id)">' + lines[i].id + ' ' + lines[i].name + ' (' +
                 lines[i].direction + ')' + '</li>';
@@ -49,7 +41,7 @@ function displayOneLine(lineId, direction) {
     let line;
     for (line of lines)
         if (line.id === lineId && line.direction === direction)
-            document.getElementById("chosenLine").innerText = line.id + ' '+ line.name + ' ' + line.direction;
+            document.getElementById("chosenLine").innerText = line.id + ' ' + line.name + ' ' + line.direction;
     let dir;
     if (direction.charAt(0) === "O")
         dir = 'W';
@@ -62,9 +54,10 @@ function displayOneLine(lineId, direction) {
             console.log("Found " + str);
             console.log(stops[line]);
             document.getElementById("allStops").innerHTML = '';
-            for (let i = 0; i < stops[line].length; i++){
-                document.getElementById("allStops").innerHTML += '<tr> <td class="stop">' + stops[line][i].name+
-                    '</td>' + '<td class="code">' + stops[line][i].id + '<td class="time"><a href="#times-tab">[...]</a></td> <td class="fav">+</td>';
+            for (let i = 0; i < stops[line].length; i++) {
+                document.getElementById("allStops").innerHTML += '<tr> <td class="stop">' +
+                    stops[line][i].name + '</td>' + '<td class="code">' + stops[line][i].id +
+                    '<td class="time"><a href="#times-tab">[...]</a></td> <td class="fav">+</td>';
             }
         }
     }
