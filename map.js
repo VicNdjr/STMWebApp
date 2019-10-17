c21f0b639020ad69ecb7a28f071b410a9af6cc81
elsadronet <edronet@enseirb-matmeca.fr>
2019-10-17 19:05:59 -0400

    points et popups

--- map.js
+++ map.js
@@ -19,3 +19,32 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     id: 'mapbox.streets',
     accessToken: 'pk.eyJ1IjoiZ3RpNTI1IiwiYSI6ImNrMW05cjRlZTBlMWIzY21uYWxpNjh2dmEifQ.9BAMULLZprP9pm2-70Q84g'
 }).addTo(mymap);
+
+
+var arret1 = [45.5, -73.6];
+var arret2 = [45.51, -73.64];
+var arret3 = [45.52, -73.6];
+var arret4 = [45.53, -73.62];
+
+var marker1 = L.marker(arret1).addTo(mymap);
+var marker2 = L.marker(arret2).addTo(mymap);
+var marker3 = L.marker(arret3).addTo(mymap);
+var marker4 = L.marker(arret4).addTo(mymap);
+
+
+var line = [
+    arret1,
+    arret2,
+    arret3,
+    arret4
+];
+
+var path = L.polyline(line, { color: '#00aeef' }).addTo(mymap);
+
+
+
+
+marker1.bindPopup("<b>Arrêt 1</b><br>Liste des prochains arrêts.");
+marker2.bindPopup("<b>Arrêt 2</b><br>Liste des prochains arrêts.");
+marker3.bindPopup("<b>Arrêt 3</b><br>Liste des prochains arrêts.");
+marker4.bindPopup("<b>Arrêt 4</b><br>Liste des prochains arrêts.");
