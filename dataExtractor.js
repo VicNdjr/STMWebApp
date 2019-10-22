1440aeb9a82a2c02f69b2386153b520419111159
VicNdjr <vic.ndjr@hotmail.fr>
2019-10-22 11:59:41 -0400

    test API

--- dataExtractor.js
+++ dataExtractor.js
@@ -1,35 +1,145 @@
+let isTimerActive = false;
+let timer;
 
-function displaylines() {
-    for (var i = 0; i < lines.length; i ++) {
-        document.getElementById("allLines").innerHTML += '<li>' + lines[i].id + ' ' + lines[i].name + ' (' +
-            lines[i].direction + ')' + '</li>';
+/**
+ * Fetch the data about all the bus lines and display it
+ */
+function fetchLines(resp) {
+    for (let i = 0; i < resp.length; i++) {
+        document.getElementById("allLines").innerHTML += '<li id="line-' + resp[i].id + '-' +
+            resp[i].direction + '" onclick="openLine(this.id)">' + resp[i].id + ' ' + resp[i].name + ' (' +
+            resp[i].direction + ')' + '</li>';
 
-        if (lines[i].category === 'local'){
-            document.getElementById("localLines").innerHTML += '<li>' + lines[i].id + ' ' + lines[i].name + ' (' +
-                lines[i].direction + ')' + '</li>';
+        if (resp[i].category === 'local') {
+            document.getElementById("localLines").innerHTML += '<li id="line-' + resp[i].id + '-' +
+                resp[i].direction + '" onclick="openLine(this.id)">' + resp[i].id + ' ' + resp[i].name + ' (' +
+                resp[i].direction + ')' + '</li>';
         }
-
-        else if (lines[i].category === 'night'){
-            document.getElementById("nightLines").innerHTML += '<li>' + lines[i].id + ' ' + lines[i].name + ' (' +
-                lines[i].direction + ')' + '</li>';
+        else if (resp[i].category === 'night') {
+            document.getElementById("nightLines").innerHTML += '<li id="line-' + resp[i].id + '-' +
+                resp[i].direction + '" onclick="openLine(this.id)">' + resp[i].id + ' ' + resp[i].name + ' (' +
+                resp[i].direction + ')' + '</li>';
         }
-        else if (lines[i].category === 'express'){
-            document.getElementById("expressLines").innerHTML += '<li>' + lines[i].id + ' ' + lines[i].name + ' (' +
-                lines[i].direction + ')' + '</li>';
+        else if (resp[i].category === 'express') {
+            document.getElementById("expressLines").innerHTML += '<li id="line-' + resp[i].id + '-' +
+                resp[i].direction + '" onclick="openLine(this.id)">' + resp[i].id + ' ' + resp[i].name + ' (' +
+                resp[i].direction + ')' + '</li>';
         }
-
-        else if (lines[i].category === 'dedicated'){
-            document.getElementById("shuttleLines").innerHTML += '<li>' + lines[i].id + ' ' + lines[i].name + ' (' +
-                lines[i].direction + ')' + '</li>';
+        else if (resp[i].category === 'dedicated') {
+            document.getElementById("shuttleLines").innerHTML += '<li id="line-' + resp[i].id + '-' +
+                resp[i].direction + '" onclick="openLine(this.id)">' + resp[i].id + ' ' + resp[i].name + ' (' +
+                resp[i].direction + ')' + '</li>';
         }
-
-        else if (lines[i].category === 'shuttleOr'){
-            document.getElementById("shuttleOrLines").innerHTML += '<li>' + lines[i].id + ' ' + lines[i].name + ' (' +
-                lines[i].direction + ')' + '</li>';
+        else if (resp[i].category === 'shuttleOr') {
+            document.getElementById("shuttleOrLines").innerHTML += '<li id="line-' + resp[i].id + '-' +
+                resp[i].direction + '" onclick="openLine(this.id)">' + resp[i].id + ' ' + resp[i].name + ' (' +
+                resp[i].direction + ')' + '</li>';
         }
+    }
+}
 
-        console.log(lines[i].category);
+/**
+ * Send all informations about the clicked line to the display function
+ * @param line Id of the line that was clicked
+ */
+function openLine(line) {
+    const splitted = line.split("-");
+    displayOneLine(splitted[1], splitted[2])
+}
+
+/**
+ * Display the line and all its stops in the table
+ * @param lineId Id of the line that was clicked
+ * @param direction Direction of the line that was clicked
+ */
+function displayOneLine(lineId, direction) {
+    let line;
+    for (line of lines) {
+        if (line.id === lineId && line.direction === direction)
+            document.getElementById("textChosenLine").innerText = line.id + ' ' + line.name + ' '
+                + line.direction;
+    }
+    // Handle the language conversion
+    let dir;
+    if (direction.charAt(0) === "O")
+        dir = 'W';
+    else
+        dir = direction.charAt(0);
+
+    const str = lineId + '-' + dir;
+
+    for (let line in stops) {
+        if (line === str) {
+            // Clear the html
+            document.getElementById("allStops").innerHTML = '';
+
+            // Display the stops
+            for (let i = 0; i < stops[line].length; i++) {
+                document.getElementById("allStops").innerHTML += '<tr> <td class="stop">' +
+                    stops[line][i].name + '</td>' + '<td class="code">' + stops[line][i].id +
+                    '<td class="time" id="' + lineId + '-' + dir + '-' + stops[line][i].id +
+                    '" onclick="displayTimer(this.id)"><a href="#times-tab">[...]</a></td> <td class="fav">+</td>';
+            }
+        }
+    }
+}
+
+/**
+ * Launch a timer to actualise the time table every 5 seconds
+ * @param stop Id of the stop that was clicked
+ */
+function displayTimer(stop) {
+    if (isTimerActive === false) {
+        displayOneStop(stop);
+
+        timer = setTimeout(function () {
+            displayOneStop(stop);
+        }, 5000);
+        isTimerActive = true;
+    }
+    else {
+        clearInterval(timer);
+        isTimerActive = false;
+
+        displayTimer(stop);
+    }
+}
+
+/**
+ * Display the stop and its timetable
+ * @param stop Id of the stop that was clicked
+ */
+function displayOneStop(stop) {
+    // Get actual date and time
+    let today = new Date();
+
+    const splitted = stop.split("-");
+    let str = splitted[0] + '-' + splitted[1];
+
+    // Display the name of the stop
+    let s;
+    for (s of stops[str]) {
+        if (s.id === splitted[2])
+            document.getElementById("textChosenStop").innerHTML = "Prochains passages pour l'arrêt " + s.name;
     }
 
+    // Clear the html
+    document.getElementById("allTimes").innerHTML = '';
 
+    // Display max 10 arrivals
+    let counter = 0;
+    for (let i = 0; i < arrivals[stop].length && counter < 10; i++) {
+        if (parseInt(arrivals[stop][i].slice(0, 2), 10) >= today.getHours() &&
+            parseInt(arrivals[stop][i].slice(2, 4), 10) >= today.getMinutes()) {
+            counter++;
+            document.getElementById("allTimes").innerHTML += '<tr> <td class="test">' +
+                arrivals[stop][i].slice(0, 2) + ' : ' + arrivals[stop][i].slice(2, 4) + '</td> </tr>';
+        }
+    }
+
+    // If no arrival have been found
+    if (counter === 0) {
+        document.getElementById("allTimes").innerHTML = '<tr>Il n\'y a plus de bus ' +
+            'aujourd\'hui ! </tr>';
+    }
 }
--- testAPI.js
+++ testAPI.js
@@ -1,47 +1,24 @@
 
-function displaylines2() {
-var xhr = new XMLHttpRequest();
-var resp = "";
+function callAPI() {
+    var xhr = new XMLHttpRequest();
+    var resp = "";
 
-xhr.open('GET', 'http://teaching-api.juliengs.ca/gti525/STMLines.py'+'?apikey=01AQ36490');
+    xhr.open('GET', 'http://teaching-api.juliengs.ca/gti525/STMLines.py'+'?apikey=01AQ36490');
 
-xhr.responseType = 'text';
+    xhr.responseType = 'text';
 
-//ASYNCHRONE
-xhr.onreadystatechange = function(event) {
-        if (this.readyState === XMLHttpRequest.DONE) {
-            if (this.status === 200) {
-                console.log("Réponse reçue: %s", this.responseText);
-                resp = JSON.parse(xhr.responseText);
-                for (var i = 0; i < resp.length; i++) {
-                    document.getElementById("allLines").innerHTML += '<li>' + resp[i].id + ' ' + resp[i].name + ' (' +
-                        resp[i].direction + ')' + '</li>';
-
-                    if (resp[i].category === 'local') {
-                        document.getElementById("localLines").innerHTML += '<li>' + resp[i].id + ' ' + resp[i].name + ' (' +
-                            resp[i].direction + ')' + '</li>';
-                    } else if (resp[i].category === 'night') {
-                        document.getElementById("nightLines").innerHTML += '<li>' + resp[i].id + ' ' + resp[i].name + ' (' +
-                            resp[i].direction + ')' + '</li>';
-                    } else if (resp[i].category === 'express') {
-                        document.getElementById("expressLines").innerHTML += '<li>' + resp[i].id + ' ' + resp[i].name + ' (' +
-                            resp[i].direction + ')' + '</li>';
-                    } else if (resp[i].category === 'dedicated') {
-                        document.getElementById("shuttleLines").innerHTML += '<li>' + resp[i].id + ' ' + resp[i].name + ' (' +
-                            resp[i].direction + ')' + '</li>';
-                    } else if (resp[i].category === 'shuttleOr') {
-                        document.getElementById("shuttleOrLines").innerHTML += '<li>' + resp[i].id + ' ' + resp[i].name + ' (' +
-                            resp[i].direction + ')' + '</li>';
-                    }
-
-                    console.log(resp[i].category);
+    //ASYNCHRONE
+    xhr.onreadystatechange = function(event) {
+            if (this.readyState === XMLHttpRequest.DONE) {
+                if (this.status === 200) {
+                    console.log("Réponse reçue: %s", this.responseText);
+                    resp = JSON.parse(xhr.responseText);
+                    fetchLines(resp);
+                } else {
+                    console.log("Status de la réponse: %d (%s)", this.status, this.statusText);
                 }
-                //displaylines2();
-            } else {
-                console.log("Status de la réponse: %d (%s)", this.status, this.statusText);
             }
-        }
-};
+    };
 
-xhr.send();
+    xhr.send();
 }
