4a7c1a7fa74f488eb986d67ac12e3f6db11aa956
VicNdjr <vic.ndjr@hotmail.fr>
2019-11-29 15:34:24 -0500

    MongoDB version 5

--- serveur/callsAPI.js
+++ serveur/callsAPI.js
@@ -4,41 +4,68 @@ module.exports = {
     fetchPositions: function (res, route, direction) {
         let url = 'http://teaching-api.juliengs.ca/gti525/STMPositions.py' +
             '?apikey=01AQ42110&route=' + route + '&direction=' + direction;
-        getData(url, res);
+        checkDb(url, res, 0);
     },
 
     fetchArrivals: function (res, route, direction, stop) {
         let url = 'http://teaching-api.juliengs.ca/gti525/STMArrivals.py' +
             '?apikey=01AQ42110&route=' + route + '&direction=' + direction + '&stopCode=' + stop;
-        getData(url, res);
+        checkDb(url, res, 0);
     },
 
     fetchStops: function (res, route, direction) {
         let url = 'http://teaching-api.juliengs.ca/gti525/STMStops.py' +
             '?apikey=01AQ42110&route=' + route + '&direction=' + direction;
-        getData(url, res);
+        checkDb(url, res, 1);
     },
 
     fetchLines: function (res) {
         let url = 'http://teaching-api.juliengs.ca/gti525/STMLines.py' + '?apikey=01AQ42110';
-        getData(url, res);
+        checkDb(url, res, 1);
     }
 };
 
-function getData(url, res) {
+function checkDb(monurl, res, test) {
+    var MongoClient = require('mongodb').MongoClient;
+    var url = "mongodb://localhost:27017/";
+    MongoClient.connect(url, function(err, db) {
+        if (err) throw err;
+        var dbo = db.db("mydb");
+        dbo.collection("url").find({myurl: monurl}).toArray(function (err, result) {
+            if (result.length > 0) {
+                var data = result[0].mydata;
+                res.end(data);
+                console.log("data récupérée");
+            } else {
+                getData(monurl, res, test, dbo);
+            }
+        });
+    });
+}
+
+function addToDb (monurl, data, dbo) {
+    var myobj = {myurl : monurl, mydata : data}; //ajouter champ user
+    dbo.collection("url").insertOne(myobj, function(err, res) {
+        if (err) throw err;
+        console.log("Ajouté : "+myobj);
+    });
+
+}
+
+function getData(url, res, test, dbo) {
     http.get(url, (resp) => {
         let data = '';
-
         // A chunk of data has been received.
         resp.on('data', (chunk) => {
             data += chunk;
         });
-
         // The whole response has been received. Print out the result.
         resp.on('end', () => {
             res.send(data);
+            if(test===1){
+                addToDb(url, data, dbo);
+            }
         });
-
     }).on("error", (err) => {
         console.log("Error: " + err.message);
     });
--- serveur/server.js
+++ serveur/server.js
@@ -1,121 +1,47 @@
-var http = require('http');
-var fs = require('fs');
-var path = require('path');
+const call = require("./callsAPI");
+const http = require('http');
+const fs = require('fs');
+const express = require('express');
+const app = express();
 
-http.createServer(function (req, res) {
-    const http = require('http');
-    if (req.url === "/") {
-        fs.readFile("../client/index.html", "UTF-8", function (err, html) {
-            res.writeHead(200, {"Content-Type": "text/html"});
-            res.end(html);
-        });
-    } else if (req.url.match("\.css$")) {
-        var cssPath = path.join(__dirname, '../client', req.url);
-        var fileStream = fs.createReadStream(cssPath, "UTF-8");
-        res.writeHead(200, {"Content-Type": "text/css"});
-        fileStream.pipe(res);
+//send js, css and images files
+app.use(express.static('../client'));
 
-    } else if (req.url.match("\.png$")) {
-        var imagePath = path.join(__dirname, '../client', req.url);
-        var fileStream = fs.createReadStream(imagePath);
-        res.writeHead(200, {"Content-Type": "image/png"});
-        fileStream.pipe(res);
-    } else if (req.url.match("\.js$")) {
-        var imagePath = path.join(__dirname, '../client', req.url);
-        var fileStream = fs.createReadStream(imagePath);
-        res.writeHead(200, {"Content-Type": "application/javascript"});
-        fileStream.pipe(res);
-    } else if (req.url === "/lines") { //TODO : Actually use this code
-        http.get('http://teaching-api.juliengs.ca/gti525/STMLines.py' + '?apikey=01AQ42110', (resp) => {
-            let data = '';
+// Index page
+app.get('/', function (req, res) {
+    fs.readFile("../client/index.html", "UTF-8", function (err, html) {
+        res.send(html);
+    });
+});
 
-            // A chunk of data has been recieved.
-            resp.on('data', (chunk) => {
-                data += chunk;
-            });
+// API page for the lines
+app.get("/lines", function (req, res) {
+    call.fetchLines(res);
+});
 
-            // The whole response has been received. Print out the result.
-            resp.on('end', () => {
-                res.setHeader('Content-Type', 'application/json');
-                res.end(data);
-            });
+// API page for the stops
+app.get("/stops/:route/:dir", function (req, res) {
+    let route = req.params.route;
+    let direction = req.params.dir;
+    call.fetchStops(res, route, direction)
+});
 
-        }).on("error", (err) => {
-            console.log("Error: " + err.message);
-        });
-    } else if (req.url.startsWith("/stops")){
-        var url_split = req.url.split('/');
-        var route = url_split[2];
-        var direction = url_split[3];
-        http.get('http://teaching-api.juliengs.ca/gti525/STMStops.py' +
-            '?apikey=01AQ42110&route=' + route + '&direction=' + direction, (resp) => {
-            let data = '';
+// API page for the arrivals
+app.get("/arrivals/:route/:dir/:stopCode", function (req, res) {
+    let route = req.params.route;
+    let direction = req.params.dir;
+    let stop = req.params.stopCode;
+    call.fetchArrivals(res, route, direction, stop);
+});
 
-            // A chunk of data has been recieved.
-            resp.on('data', (chunk) => {
-                data += chunk;
-            });
+// API page for the bus positions
+app.get("/positions/:route/:dir", function (req, res) {
+    let route = req.params.route;
+    let direction = req.params.dir;
+    call.fetchPositions(res, route, direction);
+});
 
-            // The whole response has been received. Print out the result.
-            resp.on('end', () => {
-                res.setHeader('Content-Type', 'application/json');
-                res.end(data);
-            });
-
-        }).on("error", (err) => {
-            console.log("Error: " + err.message);
-        });
-
-    } else if (req.url.startsWith("/arrivals")){
-        var url_split = req.url.split('/');
-        var route = url_split[2];
-        var direction = url_split[3];
-        var stop = url_split[4];
-        http.get('http://teaching-api.juliengs.ca/gti525/STMArrivals.py' +
-            '?apikey=01AQ42110&route=' + route + '&direction=' + direction + '&stopCode=' + stop, (resp) => {
-            let data = '';
-
-            // A chunk of data has been recieved.
-            resp.on('data', (chunk) => {
-                data += chunk;
-            });
-
-            // The whole response has been received. Print out the result.
-            resp.on('end', () => {
-                res.setHeader('Content-Type', 'application/json');
-                res.end(data);
-            });
-
-        }).on("error", (err) => {
-            console.log("Error: " + err.message);
-        });
-
-    } else if (req.url.startsWith("/positions")){
-        var url_split = req.url.split('/');
-        var route = url_split[2];
-        var direction = url_split[3];
-        http.get('http://teaching-api.juliengs.ca/gti525/STMPositions.py' +
-            '?apikey=01AQ42110&route=' + route + '&direction=' + direction, (resp) => {
-            let data = '';
-
-            // A chunk of data has been recieved.
-            resp.on('data', (chunk) => {
-                data += chunk;
-            });
-
-            // The whole response has been received. Print out the result.
-            resp.on('end', () => {
-                res.setHeader('Content-Type', 'application/json');
-                res.end(data);
-            });
-
-        }).on("error", (err) => {
-            console.log("Error: " + err.message);
-        });
-
-    } else {
-        res.writeHead(404, {"Content-Type": "text/html"});
-        res.end("No Page Found");
-    }
-
-}).listen(8080);
+app.listen(8080, function () {
+    // Print the link to ease access
+    console.log("http://localhost:8080/");
+});
