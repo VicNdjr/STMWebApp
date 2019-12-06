9607259d5b435a8c6beaa40bf77997d1d047ae37
elsadronet <edronet@enseirb-matmeca.fr>
2019-12-05 20:17:37 -0500

    éléments périmés BDD + footer

--- serveur/callsAPI.js
+++ serveur/callsAPI.js
@@ -1,35 +1,40 @@
 const http = require('http');
 
+let timeLine = 3600000; // 1h
+let timeStop = 3600000; // 1h
+let timeArrivals = 60000; // 1min
+let timeBus = 10000; // 10s
+
 module.exports = {
     fetchPositions: function (res, route, direction) {
         let url = 'http://teaching-api.juliengs.ca/gti525/STMPositions.py' +
             '?apikey=01AQ42110&route=' + route + '&direction=' + direction;
         let path = "/positions/"+route+"/"+direction;
-        getData(url,path,res,0);
+        checkDb(url,path,res,1, "positions");
     },
 
     fetchArrivals: function (res, route, direction, stop) {
         let url = 'http://teaching-api.juliengs.ca/gti525/STMArrivals.py' +
             '?apikey=01AQ42110&route=' + route + '&direction=' + direction + '&stopCode=' + stop;
         let path = "/arrivals/"+route+"/"+direction+"/"+stop;
-        getData(url,path,res,0);
+        checkDb(url,path,res,1, "arrivals");
     },
 
     fetchStops: function (res, route, direction) {
         let url = 'http://teaching-api.juliengs.ca/gti525/STMStops.py' +
             '?apikey=01AQ42110&route=' + route + '&direction=' + direction;
         let path = "/stops/"+route+"/"+direction;
-        checkDb(url, path, res, 1);
+        checkDb(url, path, res, 1, "stops");
     },
 
     fetchLines: function (res) {
         let url = 'http://teaching-api.juliengs.ca/gti525/STMLines.py' + '?apikey=01AQ42110';
         let path = "/lines";
-        checkDb(url, path, res, 1);
+        checkDb(url, path, res, 1, "lines");
     }
 };
 
-function checkDb(apiUrl, path, res, test) {
+function checkDb(apiUrl, path, res, test, type) {
     var MongoClient = require('mongodb').MongoClient;
     var url = "mongodb://localhost:27017/";
     MongoClient.connect(url, function(err, db) {
@@ -42,7 +47,18 @@ function checkDb(apiUrl, path, res, test) {
                 res.end(data);
                 console.log("data récupérée");
             } else { //sinon on fait le call à l'API
-                getData(apiUrl, path, res, test, dbo);
+                if (type == "lines") {
+                    getData(apiUrl, path, res, test, dbo, timeLine);
+                }
+                else if (type == "stops"){
+                    getData(apiUrl, path, res, test, dbo, timeStop);
+                }
+                else if (type == "arrivals"){
+                    getData(apiUrl, path, res, test, dbo, timeArrivals);
+                }
+                else {
+                    getData(apiUrl, path, res, test, dbo, timeBus);
+                }
                 console.log("nope");
             }
         });
@@ -57,7 +73,15 @@ function addToDb (path, data, dbo) {
     });
 }
 
-function getData(url, path, res, test, dbo) {
+function deleteToDb (path, dbo) {
+    var deleteEntry = {"mypath" : path};
+    dbo.collection("cache").deleteOne(deleteEntry, function(err, res) {
+        if (err) throw err;
+        console.log("Data expirée et supprimée : " + path);
+    });
+}
+
+function getData(url, path, res, test, dbo, timer) {
     http.get(url, (resp) => {
         let data = '';
         resp.on('data', (chunk) => {
@@ -67,6 +91,9 @@ function getData(url, path, res, test, dbo) {
             res.send(data);
             if(test===1){
                 addToDb(path, data, dbo);
+                setTimeout(function () {
+                    deleteToDb(path, dbo);
+                }, timer);
             }
         });
     }).on("error", (err) => {
