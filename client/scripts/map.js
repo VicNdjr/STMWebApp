0a2de87055cf82eb4d3723c59e603698025d8940
elsadronet <edronet@enseirb-matmeca.fr>
2019-12-06 08:17:29 -0500

    map favoris

--- client/scripts/map.js
+++ client/scripts/map.js
@@ -1,12 +1,20 @@
 let bus;
 let markers = [];
 let path = [];
+let pathFav = [];
 let line;
+let lineFavoris;
 let busPos = [];
+let busPosFav = [];
 let busMarker = [];
+let busMarkerFav = [];
 let nameStop;
 let timerBus;
+let timerBusFav;
 let stopArrivals;
+let listFavorites = [];
+let markersFav = [];
+let arretFav;
 
 /**
  * Display the map
@@ -65,11 +73,40 @@ function affiche_carte2() {
         busFunction();
         timerBus = setInterval(function () {
             busFunction();
-        }, 10000);
+        }, 5000);
         mymap.fitBounds([
             [parseFloat(currentStop[0].lat), parseFloat(currentStop[0].lon)],
             [parseFloat(currentStop[currentStop.length - 1].lat), parseFloat(currentStop[currentStop.length - 1].lon)]
         ]);
+
+    }
+
+    if (favorites) {
+        let lineFav = "";
+        let dirFav = "";
+        let stopFav = "";
+        for (let i = 0; i < favorites.length; i++) {
+            lineFav = favorites[i].split('-')[1];
+            dirFav = favorites[i].split('-')[2];
+            stopFav = favorites[i].split('-')[3];
+            callAPIPos(lineFav,dirFav);
+            pathFav = [];
+            for (let j = 0 ; j < listFavorites.length; j++) {
+                arretFav = [parseFloat(listFavorites[j].lat), parseFloat(listFavorites[j].lon)];
+                if (listFavorites[j].id == stopFav) {
+                    markersFav[j] = L.marker(arretFav).addTo(mymap);
+                    markersFav[j].bindPopup("<b>" + listFavorites[j].name + "</b><br>",);
+                }
+                pathFav.push(arretFav);
+            }
+            lineFavoris = L.polyline(pathFav, {color: 'green'}).addTo(mymap);
+
+            // callAPIBusFav(lineFav,dirFav);
+            // timerBusFav = setInterval(function () {
+            //     callAPIBusFav(lineFav,dirFav);
+            // }, 10000);
+        }
+
     }
 }
 
@@ -82,11 +119,21 @@ function clearMap() {
             mymap.removeLayer(markers[i]);
         }
     }
+    for (let i = 0; i < markersFav.length; i++) {
+        if (markersFav[i] !== undefined) {
+            mymap.removeLayer(markersFav[i]);
+        }
+    }
     path = [];
+    pathFav = [];
     if (line !== undefined) {
         mymap.removeLayer(line);
     }
     line = undefined;
+    if (lineFavoris !== undefined) {
+        mymap.removeLayer(lineFavoris);
+    }
+    lineFavoris = undefined;
     busPos = [];
     for (let i = 0; i < busMarker.length; i++) {
         if (busMarker[i] !== undefined) {
@@ -94,7 +141,15 @@ function clearMap() {
         }
     }
     busMarker = [];
+    // busPosFav = [];
+    // for (let i = 0; i < busMarkerFav.length; i++) {
+    //     if (busMarkerFav[i] !== undefined) {
+    //         mymap.removeLayer(busMarkerFav[i]);
+    //     }
+    // }
+    // busMarkerFav = [];
     clearInterval(timerBus);
+    // clearInterval(timerBusFav);
 }
 
 
@@ -137,6 +192,37 @@ function callAPIBus(line, dir) {
                     busMarker.push(L.marker(busPos[i], {icon: iconeBus}).addTo(mymap));
                     nameStop = stopName(bus[i].next_stop);
                     busMarker[i].bindPopup("<b>Prochain arrêt : </b><br>" + nameStop);
+                }
+            } else {
+                console.log("Statut de la réponse: %d (%s)", this.status, this.statusText);
+                document.getElementById('mapid').innerHTML = 'La carte n\'a pas pu être chargée.';
+            }
+        }
+    };
+    xhr.send();
+}
+
+
+function callAPIBusFav(line, dir) {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', 'http://localhost:8080/positions/' + line + '/' + dir);
+    xhr.responseType = 'text';
+
+    //ASYNCHRONE
+    xhr.onreadystatechange = function () {
+        if (this.readyState === XMLHttpRequest.DONE) {
+            if (this.status === 200) {
+                let bus = JSON.parse(xhr.responseText);
+                busPosFav = [];
+                for (let i = 0; i < busMarkerFav.length; i++) {
+                    if (busMarkerFav[i] !== undefined) {
+                        mymap.removeLayer(busMarkerFav[i]);
+                    }
+                }
+                busMarkerFav = [];
+                for (let i = 0; i < bus.length; i++) {
+                    busPosFav.push([parseFloat(bus[i].lat), parseFloat(bus[i].lon)]);
+                    busMarkerFav.push(L.marker(busPosFav[i], {icon: iconeBus}).addTo(mymap))
                 }
             } else {
                 console.log("Statut de la réponse: %d (%s)", this.status, this.statusText);
@@ -213,4 +299,32 @@ function displayPopUp(stop, i) {
 
     // Display the pop up
     markers[i].bindPopup("<b>" + currentStop[i].name + "</b><br>" + listArrivals);
+}
+
+
+
+function callAPIPos(lineId, dir) {
+    let xhr = new XMLHttpRequest();
+
+    xhr.open('GET', 'http://localhost:8080/stops/' + lineId + '/' + dir);
+    xhr.responseType = 'text';
+
+    //ASYNCHRONE
+    xhr.onreadystatechange = function () {
+        if (this.readyState === XMLHttpRequest.DONE) {
+            if (this.status === 200) {
+                stopList = JSON.parse(xhr.responseText);
+                for (let j = 0; j < stopList.length; j++) {
+                    // if (stopList[j].id === splitted[2]) {
+                    //     name = stopList[j].name;
+                    // }
+                    // console.log(stopList[j]);
+                    listFavorites[j] = stopList[j];
+                }
+            } else {
+                console.log("Statut de la réponse: %d (%s)", this.status, this.statusText);
+            }
+        }
+    };
+    xhr.send();
 }
